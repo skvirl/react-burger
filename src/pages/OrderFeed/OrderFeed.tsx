0cc891c98eefe4d83f64eace0c70a435ac0c38fd
Roman Lancov <skvirl@gmail.com>
2023-04-06 23:54:55 +0300

    round images trying

--- src/pages/OrderFeed/OrderFeed.tsx
+++ src/pages/OrderFeed/OrderFeed.tsx
@@ -1,7 +1,9 @@
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./OrderFeed.module.css";
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import { cachedOrderFeed } from "../../utils/data";
+import { RootState } from "../../services/store";
+import { useAppSelector } from "../../hooks/redux";
 
 const OrderFeed: FC = () => {
   const ws = new WebSocket("wss://norma.nomoreparties.space/orders/all");
@@ -55,25 +57,71 @@ const CardOrder: FC<{
 }> = ({ cardData }) => {
   const { number, ingredients, name, createdAt } = cardData;
 
+  const getStoreData = (state: RootState) => ({
+    ingredientData: state.burgerIngredients.burgerIngredients,
+  });
+  const { ingredientData, } = useAppSelector(getStoreData);
+
+  const groupedIngedients = useMemo(
+    () => {
+      if (!ingredients || !ingredientData) return [];
+
+      return ingredients
+        .reduce((unicCardIngredients: Array<{ _id: string, groupCounter: number }>, CardIngredient) => {
+
+          const unicIngredient = unicCardIngredients.find(currentUnicIngredient => currentUnicIngredient._id === CardIngredient);
+
+          if (unicIngredient === undefined) {
+            unicCardIngredients.push({ _id: CardIngredient, groupCounter: 1 });
+          } else {
+            unicIngredient.groupCounter++;
+          };
+
+          return unicCardIngredients;
+        }, [])
+
+        .map(CardIngredient => {
+          const ingredientDataElement = ingredientData.find(ingredientDataElement => CardIngredient._id === ingredientDataElement._id)
+          return ingredientDataElement === undefined ?
+            { image: '', groupCounter: CardIngredient.groupCounter, alt: '' } :
+            { image: ingredientDataElement.image_mobile, groupCounter: CardIngredient.groupCounter, name: ingredientDataElement.name }
+        }
+        )
+
+        .map(viewObject => (
+        <div className={styles.card__Placeholder}>
+          <div className={styles.card__PictureBorder} />
+          <img className={styles.card__Picture} src={viewObject.image} alt={viewObject.name} />
+        </div>
+        ))
+
+    },
+    [ingredientData, ingredients]
+  );
+
+
   return (
     <div className={styles.card}>
-      <div className={styles.cardWrapper}>
+      <div className={styles.card__Wrapper}>
         <div className={`${styles.topPanel}`}>
           <div
-            className={`text text_type_digits-default ${styles.cardNumber}`}
+            className={`text text_type_digits-default  `}
           >{`#${number}`}</div>
           <div
-            className={`text text_type_main-default text_color_inactive  ${styles.cardCreatedAt}`}
+            className={`text text_type_main-default text_color_inactive  ${styles.card__CreatedAt}`}
           >
             {createdAt}
           </div>
         </div>
-        <div className={`text text_type_main-medium   ${styles.cardName}`}>
+        <div className={`text text_type_main-medium   ${styles.card__Name}`}>
           {name}
         </div>
-        <div className={`  ${styles.bottomPanel}`}>
-          <div className={styles.cardIngredients}> 🚧 🚧 🚧 🚧 🚧 🚧 🚧 🚧</div>
-          <div className={`text text_type_digits-default  ${styles.cardPrice}`}>
+        <div className={`  ${styles.card__bottomPanel}`}>
+          <div className={styles.card__Ingredients}>
+            {groupedIngedients}
+
+          </div>
+          <div className={`text text_type_digits-default  ${styles.card__Price}`}>
             <CurrencyIcon type="primary" />
             480
           </div>
