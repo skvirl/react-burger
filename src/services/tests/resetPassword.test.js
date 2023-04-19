27604a5b88e9a9f2836eabc7821ed8d3a75efa63
John Snow <skvirl@gmail.com>
2023-04-19 13:18:55 +0300

    resetPassword async reducer test complete!

--- src/services/tests/resetPassword.test.js
+++ src/services/tests/resetPassword.test.js
@@ -1,45 +1,25 @@
-import { log } from "console";
-import {jest} from '@jest/globals';
+import { jest } from "@jest/globals";
 import { fetchResetPassword } from "../slices/resetPassword";
-// import fetchMock from "fetch-mock";
-// import thunk from "redux-thunk";
-
-// fetchMock.getOnce("burger/fetchResetPassword", {
-//   body: {
-//     resetPasswordSuccess: true,
-//     resetPasswordError: null,
-//     resetPasswordMessage: null,
-//   },
-//   headers: { "content-type": "application/json" },
-// });
 
 describe("ResetPasswordThunk", () => {
-//   afterEach(() => {
-//     fetch.restore();
-//   });
-
   it("should fetchResetPassword with resolved response", async () => {
-    const TInitialState = {
-      resetPasswordSuccess: null,
-      resetPasswordError: null,
-      resetPasswordMessage: null,
-    };
+    const response = { success: true, message: "" };
+
+    fetch.mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve(response),
+    });
 
     const dispatch = jest.fn();
     const thunk = fetchResetPassword();
-
     await thunk(dispatch, () => ({}));
 
     const { calls } = dispatch.mock;
-
-    console.log(calls);
-
     expect(calls).toHaveLength(2);
-
     const [pendingCall, rejectedCall] = calls;
-    expect(pendingCall[0].type).toBe(fetchResetPassword.pending().type);
-    expect(rejectedCall[0].type).toBe(fetchResetPassword.rejected().type);
 
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    expect(pendingCall[0].type).toBe(fetchResetPassword.pending().type);
+    expect(rejectedCall[0].type).toBe(fetchResetPassword.fulfilled().type);
+    expect(rejectedCall[0].payload).toBe(response);
   });
 });
